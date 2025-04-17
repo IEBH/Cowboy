02b76f82bf87c8fccabf57b2d549311b926c7816
Connor Forbes <connorforbes25@gmail.com>
2025-04-17 12:40:36 +1000

    TS migration: Typings for debug.ts

--- src/lib/debug.ts
+++ src/lib/debug.ts
@@ -1,9 +1,17 @@
 // lib/debug.ts
-export default function CowboyDebug(...msg) {
+export interface CowboyDebugger {
+	(...msg: any[]): void;
+	enabled: boolean;
+}
+
+const CowboyDebug: CowboyDebugger = (...msg: any[]) => {
 	if (!CowboyDebug.enabled) return;
 	console.log('COWBOY-DEBUG', ...msg.map(m =>
-		typeof m == 'string' ? m
+		typeof m === 'string' ? m
 		: JSON.stringify(m)
-	))
-}
+	));
+};
+
 CowboyDebug.enabled = false;
+
+export default CowboyDebug;
